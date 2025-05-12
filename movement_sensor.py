2c5a3d1d28091084798ba6b3b221f8dfa8030284
K1Meraat <K1Meraat@gmail.com>
2025-05-12 21:33:34 +0200

    sensor = GPIO 2

--- movement_sensor.py
+++ movement_sensor.py
@@ -1,13 +1,9 @@
-import RPi.GPIO as GPIO
-import timer
+from gpiozero import MotionSensor
+from signal import pause
 
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(15, GPIO.OUT)
-GPIO.output(15, False)
-print("15 False")
-sleep(1)
-GPIO.output(15, True)
-print("15 True")
-sleep(1)
-GPIO.cleanup()
+pir = MotionSensor(2)
+
+pir.wait_for_motion()
+print("Motion Detected")
+
