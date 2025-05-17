4d1ef2eae26c01cdd6c83d609727f88cc7a7f951
K1Meraat <K1Meraat@gmail.com>
2025-05-17 16:36:29 +0200

    added while loop to diagnoise

--- movement_sensor.py
+++ movement_sensor.py
@@ -1,9 +1,12 @@
+from time import sleep
 from gpiozero import MotionSensor
 from signal import pause
 
-
-pir = MotionSensor(2)
-
-pir.wait_for_motion()
-print("Motion Detected")
-
+sleep(61) # to calibrate the sensor
+pir = MotionSensor(13) #4th pin from left bottom
+while True:
+    
+    pir.wait_for_motion()
+    print("Motion Detected")
+    pir.wait_for_no_motion()
+    print("No Motion")
