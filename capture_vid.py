66d01c618086aae6b28af0428d5d71868f857565
K1Meraat <K1Meraat@gmail.com>
2025-05-22 12:34:34 +0200

    see what asyncio.run(f(mac_address)) does

--- capture_vid.py
+++ capture_vid.py
@@ -1,23 +1,33 @@
 import time
 import cv2
 import datetime
+import logging
 
+logging.basicConfig(filename='errors.log', level=logging.ERROR,
+        format='%(asctime)s - %(levelname)s - %(message)s')
 capture_duration = 300
-capture = cv2.VideoCapture(0)
-start_time = time.time()
-file_name = datetime.datetime.now.strftime("%Y-%m-%d_%H-%M-%S")+".mp4"
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(datetime.datetime.now()'mp4', fourcc, 15.0, (640,480))
+def record():
+    capture = cv2.VideoCapture(0)
+    if not caputre.isOpened():
+        logging.error("Webcam is not accessible")
+        capture = cv2.VideoCapture(1)
+        if not caputre.isOpened():
+            logging.error("Backup camera is also not accessible")
+            return
+    start_time = time.time()
+    file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")+".mp4"
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(datetime.datetime.now()'mp4', fourcc, 15.0, (640,480))
+    while(int(time.time()-start_time) < capture_duration:
+            ret, frame = cap_read()
+            if ret:
+                out.write(frame)
+                cv2.imshow("recording", frame)
+            else:
+                logging.error("Error reading frame from camera")
+                break
 
-while(int(time.time()-start_time) < capture_duration:
-        ret, frame = cap_read()
-        if ret:
-            out.write(frame)
-        else:
-            print("Error with ret")
-            break
 
-
-capture.release()
-out.release()
-cv2.destroyAllWindows()
+    capture.release()
+    out.release()
+    cv2.destroyAllWindows()
---
+++ test.py
@@ -0,0 +1,30 @@
+import time
+from gpiozero import MotionSensor
+from signal import pause
+import asyncio
+from bleak import BleakClient
+from gpiozero import MotionSensor
+import capture_vid
+import logging
+
+#only include errors, write down the time, what kind of message, message content
+logging.basicConfig(filename='errors.log', level=logging.ERROR,
+                format='%(asctime)s - %(levelname)s - %(message)s')
+
+#increas readability
+safe = True
+unsafe = False
+#sleep(61) # to calibrate the sensor
+pir = MotionSensor(13) #4th pin from left bottom
+
+#address of the trusted phone(bluetooth)
+address = "38:E1:3D:9F:2D:15"
+#doesn't automatically disconnect after the block ends!
+async def find_device(mac_address):
+        print(f"scanning for {mac_address}...")
+            devices = await BleakScanner.discover(timeout=10.0)
+                for d in devices:
+                    print(d)
+
+
+asyncio.run(find_device(address))
